fc9b24fbd10d3d33ed086e4a7bb1758160453275
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2020-11-15 12:51:17 +0100

    Updates

---
+++ precache-manifest.e7bc41a3441dee7495062636e479b326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31efb7398a562e5b5f4c12d8a1d4ef5b",
+    "url": "/dominoes-react/index.html"
+  },
+  {
+    "revision": "c2a126d8983be6e6673e",
+    "url": "/dominoes-react/static/css/main.808704c3.chunk.css"
+  },
+  {
+    "revision": "e9ad9a8d4f36581c0472",
+    "url": "/dominoes-react/static/js/2.cd6a968a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dominoes-react/static/js/2.cd6a968a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2a126d8983be6e6673e",
+    "url": "/dominoes-react/static/js/main.b534f276.chunk.js"
+  },
+  {
+    "revision": "903bf6467d731e4f2311",
+    "url": "/dominoes-react/static/js/runtime-main.779f2a4a.js"
+  }
+]);
